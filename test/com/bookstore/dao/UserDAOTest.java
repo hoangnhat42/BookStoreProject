be04e19283c8d20d358276ba16f86e5101254be4
hoangnhat42 <hoangnhat.contact@gmail.com>
2021-10-02 21:35:54 +0700

    first commit

---
+++ test/com/bookstore/dao/UserDAOTest.java
@@ -0,0 +1,95 @@
+package com.bookstore.dao;
+
+import static org.junit.Assert.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.bookstore.entity.Users;
+
+public class UserDAOTest {
+	private static EntityManagerFactory entityManagerFactory;
+	private static EntityManager entityManager;
+	private static UserDAO userDAO;
+	
+	@BeforeClass
+	public static void setupClass() {
+		entityManagerFactory = Persistence.createEntityManagerFactory("BookStoreWebsite");
+		entityManager = entityManagerFactory.createEntityManager();
+		
+		userDAO = new UserDAO(entityManager);
+	}
+	
+	@Test
+	public void testCreateUsers() {
+		Users user1 = new Users();
+		user1.setEmail("beck@gmail.com");
+		user1.setFullName("beck");
+		user1.setPassword("123");
+		
+		user1 = userDAO.create(user1);
+		
+		assertTrue(user1.getUserId() > 0);		
+	}
+	
+	@Test(expected = PersistenceException.class)
+	public void testCreateUsersFieldsNotSet() {
+		Users user1 = new Users();	
+		user1 = userDAO.create(user1);
+	}
+
+	@Test
+	public void testUpdateUsers() {
+		Users user = new Users();
+		user.setUserId(19);
+		user.setEmail("update@gmail.com");
+		user.setFullName("Update");
+		user.setPassword("updated");
+		
+		user = userDAO.update(user);
+		String expected = "updated";
+		String actual = user.getPassword();
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testGetUsersFound() {
+		Integer userId = 19;
+		Users user = userDAO.get(userId);
+		if (user != null) {
+			System.out.println(user.getEmail());
+		}
+		assertNotNull(user);
+	}
+	
+	@Test
+	public void testGetUserNotFound() {
+		Integer userId = 99;
+		Users user = userDAO.get(userId);
+		
+		assertNull(user);
+	}
+	
+	@Test
+	public void testDeleteUsers() {
+		Integer userId = 5;
+		userDAO.delete(userId);
+		
+		Users user = userDAO.get(userId);
+		
+		assertNull(user);
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+		entityManager.close();
+		entityManagerFactory.close();
+	}
+}
